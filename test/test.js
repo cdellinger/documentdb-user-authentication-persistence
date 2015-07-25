680d3e96017b415d337ec75d9a8e4bb150840005
Chris Dellinger <cdellinger@gmail.com>
2015-07-24 20:24:05 -0400

    Added new tests and updated existing ones to reflect new method signatures

--- test/test.js
+++ test/test.js
@@ -24,16 +24,55 @@ db.create('TWITTER', 'gdellinger', 'token2345', 'gdellinger', 'gdellinger@gmail.
 });
 */
 
+var UserSchema = require('../lib/schema');
+var tenantlessUser = new UserSchema();
+var tenantlessUserId = '';
+
+tenantlessUser.userHandle = '|||TESTUSER1|||';
+tenantlessUser.addStrategy('###TESTUSER1_TWITTER!###', 'TWITTER', '12345');
+
+
+
+var testTenant = '#TEST_TENANT#';
+var tenantUser = new UserSchema();
+var tenantUserId = '';
+
+tenantUser.userHandle = '|||TESTUSER2|||';
+tenantUser.tenant = testTenant;
+tenantUser.addStrategy('###TESTUSER2_TWITTER!###', 'TWITTER', '12345');
+
+
 
 describe('DB Test', function() {
 	before(function(){
-		console.log("BEFORE!!!!");
+
 	});
 	describe('Save With Tenant', function () {
 		it('should persist without error', function (done) {
-			db.create('TWITTER', 'adellinger', 'token12345', 'adellinger', 'adellinger@gmail.com', 'TENANT1', function(err, data){
+			db.create(tenantUser, function(err, data){
 				if (err) throw err;
+				tenantUserId = data.id;
+				done();
+			});
+		});
+	});
 
+
+	describe('Save Without Tenant', function () {
+		it('should persist without error', function (done) {
+			db.create(tenantlessUser, function(err, data){
+				if (err) throw err;
+				tenantlessUserId = data.id;
+				done();
+			});
+		});
+	});
+
+	describe('Get By Strategy With Tenant', function () {
+		it('should retrieve matching user', function (done) {
+			db.getByStrategy(tenantUser.strategies[0].type, tenantUser.strategies[0].id, testTenant, function(err, user){
+				if (err) throw err;
+				user.sid.should.equal(tenantUser.strategies[0].id);
 				done();
 			});
 		});
@@ -41,17 +80,17 @@ describe('DB Test', function() {
 
 	describe('Get By Strategy Without Tenant', function () {
 		it('should retrieve matching user', function (done) {
-			db.getByStrategy('TWITTER', 'gdellinger', function(err, user){
+			db.getByStrategy(tenantlessUser.strategies[0].type, tenantlessUser.strategies[0].id, '', function(err, user){
 				if (err) throw err;
-				user.sid.should.equal('gdellinger');
+				user.sid.should.equal(tenantlessUser.strategies[0].id);
 				done();
 			});
 		});
 	});
 
-	describe('Get By Strategy Without Tenant with Invalid Strategy Id', function () {
+	describe('Get By Strategy With Tenant with Invalid Strategy Id', function () {
 		it('should retrieve matching user', function (done) {
-			db.getByStrategy('TWITTER', 'INVALID_USER_STRATEGY_ID', function(err, user){
+			db.getByStrategy(tenantUser.strategies[0].id, 'INVALID_USER_STRATEGY_ID', tenantUser.tenant, function(err, user){
 				if (err) throw err;
 				should.equal(user, undefined);
 				done();
@@ -59,11 +98,11 @@ describe('DB Test', function() {
 		});
 	});
 
-	describe('Get By Strategy With Valid Tenant', function () {
+	describe('Get By Strategy Without Tenant with Invalid Strategy Id', function () {
 		it('should retrieve matching user', function (done) {
-			db.getByStrategy('TWITTER', 'adellinger', 'TENANT1', function(err, user){
+			db.getByStrategy(tenantUser.strategies[0].id, 'INVALID_USER_STRATEGY_ID', '', function(err, user){
 				if (err) throw err;
-				user.sid.should.equal('adellinger');
+				should.equal(user, undefined);
 				done();
 			});
 		});
@@ -71,7 +110,7 @@ describe('DB Test', function() {
 
 	describe('Get By Strategy With Invalid Tenant', function () {
 		it('should retrieve undefined value', function (done) {
-			db.getByStrategy('TWITTER', 'adellinger', 'TENANT2', function(err, user){
+			db.getByStrategy(tenantUser.strategies[0].type, tenantUser.strategies[0].id, 'INVALID_TENANT', function(err, user){
 				if (err) throw err;
 				should.equal(user, undefined);
 				done();	
@@ -80,24 +119,68 @@ describe('DB Test', function() {
 	});
 
 
-	describe('Update User', function () {
+	describe('Update User with Tenant', function () {
 		it('should update successfully', function (done) {
-			console.log('NEED TO CLEANUP');
-			db.get('d7d906d5-5276-edc3-53ea-843fca1ee2f3', function(err, user){
+			db.get(tenantUserId, function(err, user){
 				if (err) throw err;
-				user.email = 'adellinger2@gmail.com';
+				var newEmail = user.email + '1234';
+				user.email = newEmail;
 				db.update(user, function(err2, data){
-					if (err2) throw err;
-					console.log(data);
-					done();	
+					if (err2) throw err2;
+					db.get(tenantUserId, function(err3, savedUser){
+						if (err3) throw err3;
+						user.email.should.equal(newEmail);
+						done();
+					});
 				});
 			});
 		});
 	});
 
 
+	describe('Update User without Tenant', function () {
+		it('should update successfully', function (done) {
+			db.get(tenantlessUserId, function(err, user){
+				if (err) throw err;
+				var newEmail = user.email + '1234';
+				user.email = newEmail;
+				db.update(user, function(err2, data){
+					if (err2) throw err2;
+					db.get(tenantlessUserId, function(err3, savedUser){
+						if (err3) throw err3;
+						user.email.should.equal(newEmail);
+						done();
+					});
+				});
+			});
+		});
+	});
+
+	describe('Remove user with tenant', function () {
+		it('should remove matching user', function (done) {
+			db.get(tenantUserId, function(err, user){
+				if (err) throw err;
+				db.remove(user._self, function(err2, data){
+					if (err2) throw err2;
+					done();
+				});
+			});
+		});
+	});
+
+	describe('Remove user without tenant', function () {
+		it('should remove matching user', function (done) {
+			db.get(tenantlessUserId, function(err, user){
+				if (err) throw err;
+				db.remove(user._self, function(err2, data){
+					if (err2) throw err2;
+					done();
+				});
+			});
+		});
+	});
+
 	after(function(){
-		console.log('AFTER!!!');
 	});
 
 });
